5b406a710bf1a4c72046407c31d5b1939e862c31
theemadnes <alex.mattson@gmail.com>
2022-11-22 22:42:14 -0600

    templating working

--- frontend/main.go
+++ frontend/main.go
@@ -4,7 +4,13 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"text/template"
 )
+
+type IndexHtmlData struct {
+	SseServerUrl      string
+	PicoBloggerApiUrl string
+}
 
 // set default port number if env var $PORT isn't set
 func getEnv(key, fallback string) string {
@@ -16,9 +22,20 @@ func getEnv(key, fallback string) string {
 
 func main() {
 	port := getEnv("PORT", "8080")
+	sseServerUrl := getEnv("SSESERVER", "https://emojisse-4uotx33u2a-uc.a.run.app/events")
+	picobloggerApiUrl := getEnv("PICOBLOGGER-API", "https://picoblogger-4uotx33u2a-uc.a.run.app/blogposts")
 
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
+	tmpl := template.Must(template.ParseFiles("templates/indexTemplate.html"))
+
+	//fs := http.FileServer(http.Dir("./static"))
+	//http.Handle("/", fs)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := IndexHtmlData{
+			SseServerUrl:      sseServerUrl,
+			PicoBloggerApiUrl: picobloggerApiUrl,
+		}
+		tmpl.Execute(w, data)
+	})
 
 	log.Printf("Listening on :%s...", port)
 	err := http.ListenAndServe(":"+port, nil)
